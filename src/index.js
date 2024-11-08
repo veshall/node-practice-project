20c4c0ddbee21f9f563e48513547e20a99e4ce2e
veshall <vishalpaliwal51@gmail.com>
2024-11-08 16:41:56 +0530

    api err & res overiding

--- src/index.js
+++ src/index.js
@@ -1,11 +1,20 @@
 import dotenv from "dotenv";
+import { app } from "./app.js";
 import connectDB from "./db/index.js";
 
 dotenv.config({
   path: "./env",
 });
 
-connectDB();
+connectDB()
+  .then(() => {
+    app.listen(process.env.PORT || 8080, () => {
+      console.log(` Server is running at port : ${process.env.PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.log("MONGODB connection failed", error);
+  });
 
 /*
 import express from "express";
